b2c30cb668e4c7ec2a3e55f09af1c6f0587d7f72
Tedy Francy <tedy@autosmarts.co>
2024-05-05 12:45:11 +0530

    add home header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import HomeHeader from "@/components/HomeHeader";
 import React from "react";
 
 export default function Home() {
-  return <h1 className="text-red-500">Home</h1>;
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
 }
